fb0fc3d0127dc93a30f82971e4e47707003223c6
ubbebs <strusiu1001@gmail.com>
2022-08-08 20:20:10 +0200

    waiting screen

--- src/components/home_page/home.js
+++ src/components/home_page/home.js
@@ -25,46 +25,46 @@ const Home = ({data}) => {
             <h1>GUESSONG</h1>
             <p>*motywacyjny slogan (do ustalenia)*</p>
           </div>
-          <Link to="/artists"><p className="home-main-title">Artyści <span>&lt;- wybierz artystę</span></p></Link>
           {console.log(items)}
           {items ? (
             <>
-            <div class="home-main-titleline"></div>
-            <div className="home-main-container">
+              <Link to="/artists"><p className="home-main-title">Artyści <span>&lt;- wybierz artystę</span></p></Link>
+              <div class="home-main-titleline"></div>
+              <div className="home-main-container">
 
-              <HomeArtist data={data[0]}/>
-              <Link to='/artists'>
-                <div className="home-box-outline">
-                  <div className="home-box-item">
-                      <div className="home-box-item-name all">
-                        <p className="home-box-item-sign">
-                          +
-                        </p>
-                      </div>
+                <HomeArtist data={data[0]}/>
+                <Link to='/artists'>
+                  <div className="home-box-outline">
+                    <div className="home-box-item">
+                        <div className="home-box-item-name all">
+                          <p className="home-box-item-sign">
+                            +
+                          </p>
+                        </div>
+                    </div>
                   </div>
-                </div>
-              </Link>
-            </div>
-            <Link to="/albums"><p className="home-main-title">Albumy <span>&lt;- wybierz album</span></p></Link>
-            <div class="home-main-titleline"></div>
-            <div className="home-main-container">
-              <HomeAlbum data={data[1]}/>
-              <Link to='/albums'>
-                <div className="home-box-outline">
-                  <div className="home-box-item">
-                      <div className="home-box-item-name all">
-                        <p className="home-box-item-sign">
-                          +
-                        </p>
-                      </div>
+                </Link>
+              </div>
+              <Link to="/albums"><p className="home-main-title">Albumy <span>&lt;- wybierz album</span></p></Link>
+              <div class="home-main-titleline"></div>
+              <div className="home-main-container">
+                <HomeAlbum data={data[1]}/>
+                <Link to='/albums'>
+                  <div className="home-box-outline">
+                    <div className="home-box-item">
+                        <div className="home-box-item-name all">
+                          <p className="home-box-item-sign">
+                            +
+                          </p>
+                        </div>
+                    </div>
                   </div>
-                </div>
-              </Link>
-            </div>
-            <Link to="/playlists"><p className="home-main-title">Playlisty <span>&lt;- no tego jeszcze nie ma :/ (ale będzie!)</span></p></Link>
-            <div class="home-main-titleline"></div>
-            <div className="home-main-container"></div>
-          </>
+                </Link>
+              </div>
+              <Link to="/playlists"><p className="home-main-title">Playlisty <span>&lt;- no tego jeszcze nie ma :/ (ale będzie!)</span></p></Link>
+              <div class="home-main-titleline"></div>
+              <div className="home-main-container"></div>
+            </>
           ) : (
             <>
               <p className='information'>Loading...</p>
